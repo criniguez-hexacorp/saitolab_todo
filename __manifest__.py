48bf6c477133fe125ba781e5fd82b719951df2e9
criniguez <cristian.iniguez@hexacorp.com.bo>
2021-02-03 13:46:19 +0000

    manifest: name, summary, description, author and category fields modified

--- __manifest__.py
+++ __manifest__.py
@@ -2,21 +2,17 @@
 {
     'name': "saitolab_todo",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """A simple TODO""",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    'description': """TODO for testing""",
 
-    'author': "My Company",
+    'author': "criniguez",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Test',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
